9eae3f01d14b71d4961b1ad8786393c6ba48f9dd
rf5860 <robert.foy@ventyx.abb.com>
2012-04-30 11:40:57 +1000

    Remove all sql when running security script.

--- run_transform.sh
+++ run_transform.sh
@@ -2,7 +2,7 @@
 
 securityDirectory=/c/RIATransform/UML2SecurityGenerated
 files=("MSF02K_CORE" "MSF02L_CORE" "MSF02M_CORE" "MSF02N_CORE" "MSF02P_CORE" "MSF02Q_CORE" "MSF02R_CORE" "MSF02S_CORE" "MSF02T_CORE")
-rm ALL_TABLES.sql
+rm *.sql
 for i in "${files[@]}"
 do
     msxsl.exe $securityDirectory/$i.xml transform.xsl -o $i.sql
