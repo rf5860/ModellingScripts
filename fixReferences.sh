6a08a76b6a507e79d5e4cf37f312a352bb1c1c62
rf5860 <robert.foy@ventyx.abb.com>
2012-05-16 14:53:44 +1000

    Fixed script to work with all references that are invalid.

--- fixReferences.sh
+++ fixReferences.sh
@@ -1,16 +1,39 @@
 #!/bin/sh
 tempTypesFile=types.txt
+autoFix=true
+function sedeasy {
+  sed -i "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
+}
 
-grep -r "<type xmi:type=\"uml:Class\"" el_model/UMLModel/Module-8MWP > $tempTypesFile
+grep -r " href=\"[\.\./]\+Module" el_model/UMLModel/Module-8MWP > $tempTypesFile
+
 while read line; do
     file=$(echo $line | cut -d':' -f1);
     path=${file%/*};
-    expectedFile=$(echo $line | cut -d'=' -f3|cut -d'#' -f1|cut -d'"' -f2);
-    id='_'$(echo $line | cut -d'_' -f3|cut -d'?' -f1);
+    expectedFile=$(echo ${line#*href} | cut -d'=' -f2|cut -d'#' -f1|cut -d'"' -f2);
+    rawId=$(echo $line | cut -d'#' -f2|cut -d'?' -f1)
+    id='xmi:id="'$(echo $line | cut -d'#' -f2|cut -d'?' -f1)'"';
     grep -q $id ${path}'/'${expectedFile}
     if [ $? -ne 0 ]
     then
-	echo "Bad reference for:";
-	echo $line
+	actualMatch=$(grep -r $id el_model/UMLModel/*)
+	foundPath=$(echo $actualMatch|cut -d':' -f1)
+	relativePath=${expectedFile%../*}../
+	parentSep=${expectedFile%../*}../
+	relativeFound=${parentSep}${foundPath:18}
+	       
+	echo "actualMatch: $actualMatch"
+	echo "foundPath: $foundPath"
+	echo "relativePath: $relativePath"
+	echo "parentSep: $parentSep"
+	echo "relativeFound: $relativeFound"
+	echo "file: $file"
+	echo "path: $path"
+	echo "expectedFile: $expectedFile"
+	echo "rawId: $rawId"
+	echo "id: $id"
+	echo "line: $line"
+
+	sedeasy "href=\"${expectedFile}#${rawId}" "href=\"${relativeFound}#${rawId}" "$file"
     fi
 done < $tempTypesFile
