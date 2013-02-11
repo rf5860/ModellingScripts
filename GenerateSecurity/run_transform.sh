caaeb2e5984bce07de58842a769527e8e4f0fa6f
rf5860 <robert.foy@gmail.com>
2013-02-11 14:11:11 +1000

    Fixed security directory references.

--- GenerateSecurity/run_transform.sh
+++ GenerateSecurity/run_transform.sh
@@ -2,9 +2,9 @@
 
 securityDirectory=$1
 files=("MSF02K_CORE" "MSF02L_CORE" "MSF02M_CORE" "MSF02N_CORE" "MSF02P_CORE" "MSF02Q_CORE" "MSF02R_CORE" "MSF02S_CORE" "MSF02T_CORE")
-rm *.sql
+rm $securityDirectory/*.sql
 for i in "${files[@]}"
 do
-    msxsl.exe $securityDirectory/$i.xml transform.xsl -o $i.sql
-    cat $i.sql >> $securityDirectory/ALL_TABLES.sql
+    msxsl.exe $securityDirectory/$i.xml transform.xsl -o $securityDirectory/$i.sql
+    cat $securityDirectory/$i.sql >> $securityDirectory/ALL_TABLES.sql
 done
