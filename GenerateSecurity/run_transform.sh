04a0f5b04acdc91cd8d4474e191ae2f580eaa7c8
rf5860 <robert.foy@gmail.com>
2013-02-11 13:28:27 +1000

    Updated run transform to take security directory as argument

--- GenerateSecurity/run_transform.sh
+++ GenerateSecurity/run_transform.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 
-securityDirectory=/c/RIATransform/UML2SecurityGenerated
+securityDirectory=$1
 files=("MSF02K_CORE" "MSF02L_CORE" "MSF02M_CORE" "MSF02N_CORE" "MSF02P_CORE" "MSF02Q_CORE" "MSF02R_CORE" "MSF02S_CORE" "MSF02T_CORE")
 rm *.sql
 for i in "${files[@]}"
 do
     msxsl.exe $securityDirectory/$i.xml transform.xsl -o $i.sql
-    cat $i.sql >> ALL_TABLES.sql
+    cat $i.sql >> $securityDirectory/ALL_TABLES.sql
 done
